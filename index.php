09858b2c37a59dbcb5bf003d2d068d8a2e1781a9
Malcolm Littlefield <mhlittlefield@gmail.com>
2017-02-07 22:20:35 -0500

    Changed some wording in the About Me section

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <h1>Malcolm Littlefield</h1>
   <img alt="MalcolmLittlefield" src="MalcolmLittlefield.jpg">
   <h2>About Me</h2>
-  <p>I'm an aspiring developer currently working full time at SunBug Solar in Arlington Massachusetts. I'm the administrator for the monitoring systems SunBug deploys with all solar installations. I introduce customers to data acquisition systems, provide technical support, and travel across the state troubleshooting dataloggers and smart meters! I'm a passionate environmentalist, so I love working for a company that installs solar on the roofs of Massachusetts' buildings.</p>
+  <p>I'm an aspiring developer currently working full time at SunBug Solar in Arlington Massachusetts. I'm the administrator for all of SunBug's monitoring systems. I introduce customers to data acquisition systems, provide technical support, and travel across the state troubleshooting dataloggers and smart meters! I'm a passionate environmentalist, so I love working for a company that installs solar on the roofs of Massachusetts' buildings.</p>
   <p>In 2012 and 2013, when I was a senior at Middlebury College, I took introductory computer science in python, and data structures in Java. If I had discovered computer science earlier, I would have majored in it. Since then I have taken an intro ruby course at The Startup Institute in Boston, and CSCI E-12 at Harvard Extension School. DWA is the perfect continuation of my studies!</p>
   <p>In my spare time I enjoy playing American, Canadian, and Irish fiddle music! I also love hiking in the white mountains, canoeing in Maine, and traveling. In march I will visit Israel and Palestine!</p>
   <h2>Random Quote</h2>
